92eee031249ef864242f3a9186473d6347cedd6b
Rishi gupta <34707225+meetrishigupta@users.noreply.github.com>
2023-07-12 11:49:56 +0000

    12

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -36,64 +36,64 @@ export default function Blog() {
 
   return (
     <>
-     <form onSubmit={handleSubmit}>
-  <label htmlFor="title">Title:</label>
-  <input
-    type="text"
-    id="title"
-    name="title"
-    value={formData.title}
-    onChange={(e) =>
-      setFormData({
-        ...formData,
-        title: e.target.value,
-      })
-    }
-    required
-  />
-  <label htmlFor="description">Description:</label>
-  <textarea
-    rows="4"
-    cols="50"
-    type="textarea"
-    id="description"
-    name="description"
-    value={formData.description}
-    onChange={(e) =>
-      setFormData({
-        ...formData,
-        description: e.target.value,
-      })
-    }
-    required
-  />
-  <label htmlFor="image">Image Upload:</label>
-  <input
-    type="file"
-    id="image"
-    name="image"
-    onChange={handleImageChange}
-    style={{
-      backgroundColor: formData.image ? "green" : "red",
-    }}
-    required
-  />
-  <label className="fielddate" htmlFor="date">Date:</label>
-  <input
-    type="date"
-    id="date"
-    name="date"
-    value={formData.date}
-    onChange={(e) =>
-      setFormData({
-        ...formData,
-        date: e.target.value,
-      })
-    }
-    required
-  />
-  <button type="submit">Publish</button>
-</form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              title: e.target.value,
+            })
+          }
+          required
+        />
+        <label htmlFor="description">Description:</label>
+        <textarea
+          rows="4"
+          cols="50"
+          type="textarea"
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              description: e.target.value,
+            })
+          }
+          required
+        />
+        <label htmlFor="image">Image Upload:</label>
+        <input
+          type="file"
+          id="image"
+          name="image"
+          onChange={handleImageChange}
+          style={{
+            backgroundColor: formData.image ? "green" : "red",
+          }}
+          required
+        />
+        <label className="fielddate" htmlFor="date">Date:</label>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          value={formData.date}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              date: e.target.value,
+            })
+          }
+          required
+        />
+        <button type="submit">Publish</button>
+      </form>
 
       <h1 className="latesth1">
         Latest Posts
@@ -102,28 +102,28 @@ export default function Blog() {
         <div className="wrapper" key={index}>
           <div className="image-wrapper">
             <img
-            src={URL.createObjectURL(i.image)}
-            alt="Uploaded"
-          />
-          <div className="datewrapper"> 
-          <p>
-              {i.date}
+              src={URL.createObjectURL(i.image)}
+              alt="Uploaded"
+            />
+            <div className="datewrapper">
+              <p>
+                {i.date}
               </p>
+            </div>
+
           </div>
-         
-            </div>
-            <div className="content-wrapper">
-              <h2>{i.title}</h2>
-              <p>{i.description}</p>
+          <div className="content-wrapper">
+            <h2>{i.title}</h2>
+            <p>{i.description}</p>
 
-              <a className="delelteimage" href="#">Delete</a>
+            <a className="delelteimage" href="#">Delete</a>
 
-            </div>
-           
-          
-             
+          </div>
+
+
+
         </div>
-      ))} 
+      ))}
 
     </>
   );
